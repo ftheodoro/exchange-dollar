2cc58fb0c6e0e8756b59b0b49658022ad6964f41
Fabricio Theodoro <fabricio.theodoro.ti@gmail.com>
2023-06-01 11:26:46 -0300

    create client to consuming server

---
+++ client/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*300)
+	defer cancel()
+
+	requrl, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	req, err := http.DefaultClient.Do(requrl)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var exchangeRate string
+
+	if err := json.Unmarshal(res, &exchangeRate); err != nil {
+		panic(err)
+	}
+	saveDataFile(exchangeRate)
+	io.Copy(os.Stdout, req.Body)
+}
+
+func saveDataFile(exchange string) {
+	file, err := os.Create("exchangerate.log")
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(exchange)
+	dolarRate := fmt.Sprintf(" Dólar: %s", exchange)
+	_, err = file.WriteString(dolarRate)
+	if err != nil {
+		panic(err)
+	}
+
+}
--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"time"
 
@@ -12,77 +13,98 @@ import (
 )
 
 const CoinName = "USDBRL"
-const TimeRequest = 200
+const TimeAPIRequest = 200
 const TimeSaveDataBase = 10
+const TimeClientRequest = 300
 
 func main() {
-
-	http.HandleFunc("/", Index)
+	log.Println("server started")
+	http.HandleFunc("/cotacao", Index)
 	http.ListenAndServe(":8080", nil)
 
 }
 func Index(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*TimeSaveDataBase)
-	defer cancel()
-	jsonData, err, httpStatus := requestExhange()
-	if err != nil {
-		w.WriteHeader(httpStatus)
-		return
-	}
-
-	var exchangeRate model.ExchangeRate
 	w.Header().Set("Content-Type", "application/json")
-	if err = json.Unmarshal(jsonData, &exchangeRate); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	db, err := config.ConnDB()
 
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error creating database."))
+	select {
+	case <-time.After(time.Millisecond * TimeClientRequest):
 
-		return
-	}
-	err = db.WithContext(ctx).Create(exchangeRate).Error
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("timeout exceeded for saving to database"))
-		return
-	}
+		log.Println("client-initiated request")
+		defer log.Println("finish request ")
 
-	json.NewEncoder(w).Encode(exchangeRate)
+		jsonData, httpStatus, err := requestExhange()
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(httpStatus)
+			return
+		}
+
+		var exchangeRate model.ExchangeRate
+
+		if err = json.Unmarshal(jsonData, &exchangeRate); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		db, err := config.ConnDB()
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("error creating database."))
+
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*TimeSaveDataBase)
+		defer cancel()
+		err = db.WithContext(ctx).Create(exchangeRate).Error
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("timeout exceeded for saving to database"))
+			return
+		}
+
+		json.NewEncoder(w).Encode(exchangeRate.Bid)
+	case <-r.Context().Done():
+		log.Println("request cancel client")
+
+	}
 
 }
 
-func requestExhange() ([]byte, error, int) {
+func requestExhange() ([]byte, int, error) {
 	var url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*TimeRequest)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*TimeAPIRequest)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		return []byte(""), err, http.StatusBadRequest
+		return []byte(""), http.StatusBadRequest, err
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return []byte(""), err, http.StatusRequestTimeout
+		log.Println("Timeout request")
+		return []byte(""), http.StatusRequestTimeout, err
 	}
 	defer resp.Body.Close()
 
 	var apiData map[string]interface{}
 
 	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error read body request")
+		return []byte(""), http.StatusBadRequest, err
+	}
 
 	if err = json.Unmarshal(body, &apiData); err != nil {
-		return []byte(""), err, http.StatusBadRequest
+		log.Println("Error unmarshal body")
+		return []byte(""), http.StatusBadRequest, err
 	}
 
 	jsonData, err := json.Marshal(apiData[CoinName])
 
 	if err != nil {
-		return []byte(""), err, http.StatusBadGateway
+		log.Println("Error marshal body")
+		return []byte(""), http.StatusBadGateway, err
 	}
-	return jsonData, nil, http.StatusOK
+	return jsonData, http.StatusOK, nil
 }
